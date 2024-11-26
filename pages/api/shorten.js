18b49ce87c06c29eb9bafe0b96da0998c1fca62d
Madhur Jaripatke <madhur03mj@gmail.com>
2024-11-27 00:48:44 +0530

    Add edge runtime export

--- pages/api/shorten.js
+++ pages/api/shorten.js
@@ -1,32 +1,49 @@
-import dbConnect from '../../utils/db';
-import Url from '../../models/url';
-import { nanoid } from 'nanoid';
+import dbConnect from "../../utils/db";
+import Url from "../../models/url";
+import { nanoid } from "nanoid";
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') { return res.status(405).json({ message: 'Method not allowed' }); }
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method not allowed" });
+  }
   const { originalUrl, alias, expirationDate, scheduledDate } = req.body;
   // console.log(req.body);
-  if (!originalUrl) { return res.status(400).json({ message: 'Original URL is required' }); }
+  if (!originalUrl) {
+    return res.status(400).json({ message: "Original URL is required" });
+  }
   const dotCount = (originalUrl.match(/\./g) || []).length;
-  if (dotCount <= 0) { return res.status(400).json({ message: 'Invalid URL or Domain' }); }
+  if (dotCount <= 0) {
+    return res.status(400).json({ message: "Invalid URL or Domain" });
+  }
 
   await dbConnect();
   try {
     const existingUrl = await Url.findOne({ originalUrl });
     if (existingUrl) {
-      return res.status(400).json({ message: 'This URL has already been shortened' });
+      return res
+        .status(400)
+        .json({ message: "This URL has already been shortened" });
     }
 
     // Check if alias already exists (Only if alias is provided)
     if (alias) {
       const existingAlias = await Url.findOne({ alias });
       if (existingAlias) {
-        return res.status(400).json({ message: 'Custom alias already in use' });
+        return res.status(400).json({ message: "Custom alias already in use" });
       }
     }
 
-    if (expirationDate && new Date(expirationDate) > new Date(Date.now() + 2 * 365 * 24 * 60 * 60 * 1000)) {
-      return res.status(400).json({ message: 'Expiration date cannot be more than 2 years from the current date' });
+    if (
+      expirationDate &&
+      new Date(expirationDate) >
+        new Date(Date.now() + 2 * 365 * 24 * 60 * 60 * 1000)
+    ) {
+      return res
+        .status(400)
+        .json({
+          message:
+            "Expiration date cannot be more than 2 years from the current date",
+        });
     }
 
     const expiration = expirationDate;
@@ -53,9 +70,12 @@ export default async function handler(req, res) {
     const createdUrl = await Url.findById(newUrl._id);
     console.log(createdUrl);
 
-    res.status(201).json({ shortenUrl: `${process.env.BASE_URL}/${shortenUrl}` });
+    res
+      .status(201)
+      .json({ shortenUrl: `${process.env.BASE_URL}/${shortenUrl}` });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Internal server error' });
+    res.status(500).json({ message: "Internal server error" });
   }
 }
+export const runtime = "edge";
